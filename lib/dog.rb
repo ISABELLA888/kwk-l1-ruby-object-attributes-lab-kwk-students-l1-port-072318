9141f00c5b11c62a020e89d882e8a9ba70c1630b
ISABELLA888 <ipcastro888@gmail.com>
2018-07-26 23:15:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
 # dog.rb
+
+class Dog
+  def initialize(bark, sit, name, breed)
+    @bark = bark
+    @sit = sit
+    @name = name
+    @breed = breed
+  end
+  
+  def bark
+    @bark
+  end
+  
+  def sit
+    @sit
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
+
+dog = Dog.new("Woof!", "The Dog is sitting", "Bob", "Pitt Bull")  
+
+puts "This dog is a #{dog.breed}."
+
+
+
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,36 @@
 # person.rb
+
+class Person
+  def initialize(talk, walk, name, job)
+    @talk = talk
+    @walk = walk
+    @name = name
+    @job = job 
+  end
+  
+  def talk
+    @talk
+  end
+  
+  def walk
+    @walk
+  end
+  
+  def name 
+    @name
+  end 
+  
+  def job
+    @job
+  end 
+end
+
+person = Person.new("Hello World!", "The Person is walking", "Billy", "Musician")
+
+puts person.walk
+puts person.talk
+puts "This is #{person.name}."
+puts person.job
+
+
+
